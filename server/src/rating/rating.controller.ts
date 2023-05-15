c0ec61f6b3136855f00ca5580dec7573f9f7a383
luvlqq <lualgod92@gmail.com>
2023-05-15 18:02:17 +0300

    add swagger and create a basic doc

--- server/src/rating/rating.controller.ts
+++ server/src/rating/rating.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { RatingService } from './rating.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Rating')
 @Controller('rating')
 export class RatingController {
   constructor(private readonly ratingService: RatingService) {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get event rating' })
   getEventRating() {}
 
   @Post(':id')
+  @ApiOperation({ summary: 'Set event rating by user' })
   setReview() {}
 }
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all users' })
   getAllUsers() {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get user by id' })
   getUserById() {}
 }
